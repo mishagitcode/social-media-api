02d28ec8aeb4ecbba0cfa4cbc6b29468584b5adc
Misha Zubritsky <zubritsky.misha@gmail.com>
2026-03-26 12:04:34 +0200

    Implement FollowViewSet, LikeViewSet, CommentViewSet, HashtagViewSet, PostViewSet, ProfileViewSet, UserViewSet

--- interactions/views.py
+++ interactions/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from rest_framework import mixins, permissions, viewsets
 
-# Create your views here.
+from config.permissions import IsOwnerOrReadOnly
+from interactions.models import Comment, Like
+from interactions.serializers import CommentSerializer, LikeSerializer
+
+
+class LikeViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet,
+):
+    queryset = Like.objects.select_related("user", "post", "post__author").all()
+    serializer_class = LikeSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+    owner_field = "user"
+
+    def get_queryset(self):
+        queryset = self.queryset
+        user_id = self.request.query_params.get("user")
+        post_id = self.request.query_params.get("post")
+
+        if user_id:
+            queryset = queryset.filter(user_id=user_id)
+        if post_id:
+            queryset = queryset.filter(post_id=post_id)
+
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.select_related("author", "post", "post__author").all()
+    serializer_class = CommentSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+    owner_field = "author"
+
+    def get_queryset(self):
+        queryset = self.queryset
+        post_id = self.request.query_params.get("post")
+        author_id = self.request.query_params.get("author")
+
+        if post_id:
+            queryset = queryset.filter(post_id=post_id)
+        if author_id:
+            queryset = queryset.filter(author_id=author_id)
+
+        return queryset
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import permissions, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
-# Create your views here.
+from config.permissions import IsOwnerOrReadOnly
+from users.serializers import UserSerializer
+
+
+User = get_user_model()
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by("id")
+    serializer_class = UserSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+    owner_field = "self"
+
+    def get_permissions(self):
+        if self.action == "create":
+            return [permissions.AllowAny()]
+        return [permission() for permission in self.permission_classes]
+
+    @action(
+        detail=False,
+        methods=["get"],
+        permission_classes=[permissions.IsAuthenticated],
+    )
+    def me(self, request):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
