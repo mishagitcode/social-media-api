292c2081a0c9bd729f9182400a65c0fcaa520651
Misha Zubritsky <zubritsky.misha@gmail.com>
2026-03-26 12:06:43 +0200

    Implement url routing and swagger docs

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,37 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework.routers import DefaultRouter
+
+from follows.views import FollowViewSet
+from interactions.views import CommentViewSet, LikeViewSet
+from posts.views import HashtagViewSet, PostViewSet
+from profiles.views import ProfileViewSet
+from users.views import UserViewSet
+
+router = DefaultRouter()
+router.register("users", UserViewSet, basename="user")
+router.register("profiles", ProfileViewSet, basename="profile")
+router.register("hashtags", HashtagViewSet, basename="hashtag")
+router.register("posts", PostViewSet, basename="post")
+router.register("follows", FollowViewSet, basename="follow")
+router.register("likes", LikeViewSet, basename="like")
+router.register("comments", CommentViewSet, basename="comment")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+    path("api/", include(router.urls)),
+    path("api/token/", obtain_auth_token, name="api-token"),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
