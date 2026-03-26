312976f454a71debe3acfb18d6ff387cec5f1ba6
Misha Zubritsky <zubritsky.misha@gmail.com>
2026-03-26 11:35:56 +0200

    Implement Comment and Like models

--- interactions/models.py
+++ interactions/models.py
@@ -1,3 +1,50 @@
+from django.conf import settings
 from django.db import models
+from posts.models import Post
 
-# Create your models here.
+
+class Like(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="likes"
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="likes"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["user", "post"],
+                name="unique_post_like"
+            )
+        ]
+
+    def __str__(self):
+        return f"{self.user} likes post {self.post_id}"
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["created_at"]
+
+    def __str__(self):
+        return f"Comment #{self.pk} by {self.author}"
