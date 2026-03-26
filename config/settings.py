ce506a9aaa5e8db4c57dafd3569f8df4e109bd7b
Misha Zubritsky <zubritsky.misha@gmail.com>
2026-03-26 11:32:25 +0200

    Apply the 'Black' formatting style to config files

--- config/settings.py
+++ config/settings.py
@@ -41,11 +41,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-
     "rest_framework",
     "rest_framework.authtoken",
     "drf_spectacular",
-
     "users",
     "profiles",
     "follows",
